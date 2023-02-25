6629fdd9ca239c1fa5403087ba869cf464af8166
InKyoJeong <viapolar@naver.com>
2023-02-25 21:35:02 +0900

    fix: fix generator

--- src/generator.ts
+++ src/generator.ts
@@ -1,16 +1,20 @@
 const fs = require("fs");
+const dir = `${__dirname}/snippets/`;
 
-const readFile = (path: string) =>
-  fs
-    .createReadStream(path)
-    .on("data", (data: Buffer) => createJsonFile(data.toString()));
+const data = {};
 
-const createJsonFile = (data: string) => {
-  return fs.appendFile("./snippets.json", JSON.stringify(data, null, 4), () => {
-    console.log("finish");
+fs.readdir(dir, (err: Error, files: any) => {
+  return new Promise((resolve, reject) => {
+    if (err) {
+      reject(err);
+    }
+
+    files.forEach((file: string) => {
+      const content = require(`${dir}${file}`);
+      Object.assign(data, content);
+    });
+    resolve(data);
+  }).then((data) => {
+    fs.writeFileSync("./snippets.json", JSON.stringify(data));
   });
-};
-
-["js", "react-js", "react-ts"].forEach((file: string) =>
-  readFile(`./snippets/${file}.json`)
-);
+});
