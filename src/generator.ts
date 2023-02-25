2616f2a26a5cbaa3785e09735be80d5a43b941b7
InKyoJeong <viapolar@naver.com>
2023-02-25 19:03:15 +0900

    fix: fix generator to append file

--- src/generator.ts
+++ src/generator.ts
@@ -6,7 +6,9 @@ const readFile = (path: string) =>
     .on("data", (data: Buffer) => createJsonFile(data.toString()));
 
 const createJsonFile = (data: string) => {
-  return fs.writeFileSync("./snippets.json", JSON.stringify(data, null, 4));
+  return fs.appendFile("./snippets.json", JSON.stringify(data, null, 4), () => {
+    console.log("finish");
+  });
 };
 
 ["js", "react-js", "react-ts"].forEach((file: string) =>
