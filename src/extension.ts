6bb9588e4bf1b6e68576aa4a93d08ff63459c514
InKyoJeong <viapolar@naver.com>
2023-02-27 21:30:54 +0900

    refactor: delete unnecessary code

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,5 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "simple-snippets.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from simple-snippets!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
-}
+export function activate(context: vscode.ExtensionContext) {}
 
 export function deactivate() {}
